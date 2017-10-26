aa0828235acdd18a1c520524c4469ae6a5194bbb
toobler <toobler@ubuntu1.dcm4che.org>
2017-10-26 16:02:56 +0530

    Changes executed

--- Streamsdemo.js
+++ Streamsdemo.js
@@ -22,22 +22,4 @@ readerStream.on('err',function(chunk){
 throw err;
 });
 */
-
-
-//var data='#######OUTPUT#######'
-var writerStream=fs.createWriteStream('newSampleClone.txt');
-/*writerStream.write(data,'UTF-8');
-writerStream.end();
-writerStream.on('finish', function() {
-    console.log("Write completed.");
-});
-
-writerStream.on('error', function(err){
-   console.log(err.stack);
-});*/
-
-
-readerStream.pipe(writerStream);
-console.log("Program Ended");
-
-//console.log('\nEnd!');
+//ghjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
